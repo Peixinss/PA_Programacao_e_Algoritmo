9b58c8b7986e7620df0f50a44be21c96c6ac5518
Peixinss <renan.a.peixinho@gmail.com>
2026-05-29 22:32:05 -0300

    Array list

--- Array/ArrayJava/src/App.java
+++ Array/ArrayJava/src/App.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> nomes = new ArrayList<>();
+
+        //Adiciona elementos a lista add()
+        nomes.add("Aluno");
+        nomes.add("Renan");
+        nomes.add("Peixinho");
+        
+        //Acessar o elemento get(indice)
+        //System.out.println(nomes.get(2));
+
+        //Alterar um item
+        //nomes.set(0, "Lindo");
+        //System.out.println(nomes);
+
+        //Remover um item da lista
+        //nomes.add("Remover este");
+        //System.out.println(nomes);
+        //nomes.remove(3);
+        //System.out.println(nomes);
+
+        //Tamanho da lista
+        //System.out.println(nomes.size());
+
+        //6 Apagar a lista
+        //nomes.clear();
+        //System.out.println(nomes);
+
+        //verificar se existe
+        System.out.println(nomes.contains("lokolol"));
+
     }
 }
