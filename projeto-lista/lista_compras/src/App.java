0b9eecb8a48dbac7e7e01a1da559abe574c896e3
peixinss <renan.a.peixinho@gmail.com>
2026-06-27 15:55:40 -0300

    termino do trabalho de PA

--- projeto-lista/lista_compras/src/App.java
+++ projeto-lista/lista_compras/src/App.java
@@ -5,120 +5,136 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
        Scanner inUser = new Scanner(System.in);
-       ArrayList<String> listaTarefas = new ArrayList<>();
+       ArrayList<String> lista = new ArrayList<>();
        
-       int opcaoLista = 0;
-       int opcao = 0;
-       String novaTarefa = "";
-       
-       
-            do{
-                try{
-                    System.out.println("""
-                    Bem vindos ao programa lista de tarefas
+        int opcaoLista = 0;
+        int opcao = 0;
 
+        do {
+            try {
+                System.out.println("""
+                        Bem-vindo ao programa Lista de Tarefas
 
-                    Escolha a opção desejada:
+                        Escolha a opção desejada:
 
+                        1 - Criar uma nova lista de tarefas
+                        2 - Sair
+                        """);
 
-                    1 - Criar uma nova lista de tarefas
-                    2 - Sair
+                opcaoLista = inUser.nextInt();
+                inUser.nextLine();
 
+                switch (opcaoLista) {
 
-                    """);
-                    opcaoLista = inUser.nextInt();
-                    inUser.nextLine();
+                    case 1 -> {
+                        System.out.println("Lista criada com sucesso!");
 
+                        do {
+                            System.out.println("""
+                                    Escolha a opção desejada:
 
-                    switch(opcaoLista){
-                        case 1 -> {
-                            System.out.println("Lista Criada com sucesso");
+                                    1 - Adicionar um item
+                                    2 - Deletar a lista
+                                    3 - Ver a lista
+                                    4 - Alterar um item
+                                    5 - Remover um item
+                                    6 - Sair
+                                    """);
 
+                            opcao = inUser.nextInt();
+                            inUser.nextLine();
 
-                            do{
-                                System.out.println("""
-                                        Escolha a opção desejada:
-                                        1 - Adicionar um item da lista
-                                        2 - Deletar sua Lista
-                                        3 - Ver a sua lista
-                                        4 - Alterar um item da Lista
-                                        5 - Remover 1 item
-                                        6 - Sair
-                                        """);
-                                   
-                                opcao = inUser.nextInt();
-                                inUser.nextLine();
-                                   
-                                switch(opcao){
+                            switch (opcao) {
+
                                 case 1 -> {
-                                    System.out.println("Você escolheu criar um item da lista ");
-                                    System.out.println("Digite o nome da atividade ");                                    
-
-
-                                    novaTarefa = inUser.nextLine();
-                                    listaTarefas.add(novaTarefa);
-
-
+                                    System.out.println("Digite o nome do item:");
+                                    String novaTarefa = inUser.nextLine();
+                                    lista.add(novaTarefa);
+                                    System.out.println("Item adicionado com sucesso!");
                                 }
+
                                 case 2 -> {
-                                System.out.println("Voce escolheu deletar a lista");
-                                listaTarefas.clear();
-                                System.out.println(listaTarefas);
-                                System.out.println("lista deletada com sucesso");
+                                    lista.clear();
+                                    System.out.println("Lista deletada com sucesso!");
                                 }
-                                
-                                case 3 ->{
-                                    System.out.println("Você escolheu ver a lista");
-                                    for(String item : listaTarefas){
-                                        System.out.printf("%s \n", item);
+
+                                case 3 -> {
+                                    if (lista.isEmpty()) {
+                                        System.out.println("A lista está vazia.");
+                                    } else {
+                                        System.out.println("Lista de tarefas:");
+                                        for (int i = 0; i < lista.size(); i++) {
+                                            System.out.printf("%d - %s%n", i, lista.get(i));
+                                        }
                                     }
                                 }
-                                case 4 ->{
-                                    System.out.println("Voce escolheu Alterar um item");
-                                    System.out.println();
+
+                                case 4 -> {
+                                    if (lista.isEmpty()) {
+                                        System.out.println("A lista está vazia.");
+                                    } else {
+
+                                        for (int i = 0; i < lista.size(); i++) {
+                                            System.out.printf("%d - %s%n", i, lista.get(i));
+                                        }
+
+                                        System.out.println("Digite o número do item que deseja alterar:");
+                                        int posicaoAlterar = inUser.nextInt();
+                                        inUser.nextLine();
+
+                                        if (posicaoAlterar >= 0 && posicaoAlterar < lista.size()) {
+                                            System.out.println("Digite o novo item:");
+                                            String novoProduto = inUser.nextLine();
+                                            lista.set(posicaoAlterar, novoProduto);
+                                            System.out.println("Item alterado com sucesso!");
+                                        } else {
+                                            System.out.println("Posição inválida.");
+                                        }
+                                    }
                                 }
-                                    
+
                                 case 5 -> {
-                                    System.out.println("Você escolheu remover um item:");
-                                    System.out.println("Escolha qual item remover:");
-                                    for(String item : listaTarefas){
-                                        System.out.printf("%s \n", item, "\n");
-                                    }
-                                    String itvRemover = inUser.nextLine();
+                                    if (lista.isEmpty()) {
+                                        System.out.println("A lista está vazia.");
+                                    } else {
 
-                                    boolean removido = listaTarefas.remove(itvRemover);
+                                        for (int i = 0; i < lista.size(); i++) {
+                                            System.out.printf("%d - %s%n", i, lista.get(i));
+                                        }
 
-                                if (removido) {
-                                    System.out.println("Item removido com sucesso!");
-                                } else {
-                                    System.out.println("Item não encontrado.");
+                                        System.out.println("Digite o número do item que deseja remover:");
+                                        int posicaoRemover = inUser.nextInt();
+                                        inUser.nextLine();
+
+                                        if (posicaoRemover >= 0 && posicaoRemover < lista.size()) {
+                                            lista.remove(posicaoRemover);
+                                            System.out.println("Item removido com sucesso!");
+                                        } else {
+                                            System.out.println("Posição inválida.");
+                                        }
                                     }
                                 }
 
+                                case 6 -> System.out.println("Saindo da lista...");
 
-                                }
-                                
-                           
-                             }while(opcao !=6);                    
+                                default -> System.out.println("Opção inválida.");
+                            }
 
-
-
-
-                                                                                                                   
-                        }
-
-                        case 2 -> System.out.println("Obrigado por usar o nosso sistema");
-                        default -> System.out.println("opção invalida");
+                        } while (opcao != 6);
                     }
-           
-                }catch(Exception erro){
-                    System.out.println("Erro!!! Entrada invalida");
-                    inUser.nextLine();
-                    opcaoLista = 1;
+
+                    case 2 -> System.out.println("Obrigado por usar o nosso sistema!");
+
+                    default -> System.out.println("Opção inválida.");
                 }
-           
-            }while(opcaoLista != 2);        
-       
-       inUser.close();
+
+            } catch (Exception erro) {
+                System.out.println("Erro! Entrada inválida.");
+                inUser.nextLine();
+            }
+
+        } while (opcaoLista != 2);
+
+        inUser.close();
     }
 }
